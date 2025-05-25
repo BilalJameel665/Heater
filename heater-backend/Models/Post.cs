b715ca8a5e615acc5c91aa21e2cf77c9afc4cf5f
Poobly <79822237+Poobly@users.noreply.github.com>
2025-05-25 13:49:56 -0400

    finished post model

--- heater-backend/Models/Post.cs
+++ heater-backend/Models/Post.cs
@@ -1,18 +1,28 @@
-
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace heater_backend.Models
 {
 	class Post
 	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		[Key]
 		public Guid Id { get; set; }
 
+		[Required]
 		[MaxLength(500)]
-		public required string Text { get; set; }
+		public string Text { get; set; } = string.Empty;
 
-		public required User Author { get; set; }
+		[Required]
+		public User Author { get; set; }
+		[Required]
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+		[Required]
+		public required Metrics Metrics { get; set; }
 
 		public ICollection<Comment> Comments { get; set; } = [];
+
 	}
 }
