4f99c13d56f2ac8926292088b38da9cc317e6fdb
Bilal-jameel <44616769+BilalJameel665@users.noreply.github.com>
2025-06-23 14:13:35 -0400

    signup page

--- heater-frontend/src/index.tsx
+++ heater-frontend/src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 const App: React.FC = () => {
 	return <h1>Hello from React + TypeScript + Webpack!</h1>;
