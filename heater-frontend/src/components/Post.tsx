7a5a5b6e92b567b2b89012b6a52b38e8ce07d8f2
Poobly <79822237+Poobly@users.noreply.github.com>
2025-07-19 18:31:29 -0400

    basic styling

--- heater-frontend/src/components/Post.tsx
+++ heater-frontend/src/components/Post.tsx
@@ -1,19 +1,16 @@
 import styles from "./Post.module.css";
 
-
-
 export default function Post({ post }: { post: Post }) {
 	return (
 		<div className={styles["post-con"]}>
 			<div className={styles["post-header"]}>
 				<div className={styles["post-author"]}>{post.author}</div>
-				<div className={styles["post-options"]}>Options</div>
+				<div className={styles["post-options"]}>
+					<span className="material-symbols-outlined">more_horiz</span>
+				</div>
 			</div>
 			<p className={styles["post-text"]}>{post.text}</p>
-			<div className={styles["post-analytics"]}>
-				{/* likes, comments */}
-				
-			</div>
+			<div className={styles["post-analytics"]}>{/* likes, comments */}</div>
 		</div>
 	);
 }
