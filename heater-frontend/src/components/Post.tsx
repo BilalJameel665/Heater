366069e8226127060cb7bff69cc4624a5f90dd59
Poobly <79822237+Poobly@users.noreply.github.com>
2025-07-20 15:44:46 -0400

    added/modified styling to homepage and post components

--- heater-frontend/src/components/Post.tsx
+++ heater-frontend/src/components/Post.tsx
@@ -5,25 +5,27 @@ import CommentIcon from "../assets/comment.svg";
 import ViewIcon from "../assets/visibility.svg";
 import RepostIcon from "../assets/repost.svg";
 import ShareIcon from "../assets/share.svg";
+import OptionIcon from "../assets/more_horiz.svg";
+import { Link } from "react-router";
 
 export default function Post({ post }: { post: Post }) {
 	return (
 		<div className={styles["post-con"]}>
 			<div className={styles["post-header"]}>
-				<div className={styles["post-author"]}>{post.author}</div>
+				<div className={styles["post-author"]}><Link to={`/${post.author}`}>{post.author}</Link></div>
 				<div className={styles["post-options"]}>
-					<span className="material-symbols-outlined">more_horiz</span>
+					<button><OptionIcon /></button>
 				</div>
 			</div>
 			<p className={styles["post-text"]}>{post.text}</p>
 			<hr />
 			<div className={styles["post-analytics"]}>
-				<span><ThumbsUpIcon />{post.metrics.likeCount}</span>
-				<span><ThumbsDownIcon />{post.metrics.dislikeCount}</span>
-				<span><CommentIcon />{post.metrics.commentCount}</span>
-				<span><RepostIcon />{post.metrics.viewCount}</span>
-				<span><ViewIcon />{post.metrics.repostCount}</span>
-				<span><ShareIcon /></span>
+				<button><ThumbsUpIcon />{post.metrics.likeCount}</button>
+				<button><ThumbsDownIcon />{post.metrics.dislikeCount}</button>
+				<button><CommentIcon />{post.metrics.commentCount}</button>
+				<button><RepostIcon />{post.metrics.viewCount}</button>
+				<button><ViewIcon />{post.metrics.repostCount}</button>
+				<button><ShareIcon /></button>
 			</div>
 		</div>
 	);
